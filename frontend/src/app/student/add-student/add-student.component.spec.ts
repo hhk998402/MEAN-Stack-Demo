696840359aa8ecfd55c5f44f85ecec2071d09977
hhk998402 <hemant.hari.kumar@gmail.com>
2020-10-10 13:50:27 +0000

    Add settings to run ng test

--- frontend/src/app/student/add-student/add-student.component.spec.ts
+++ frontend/src/app/student/add-student/add-student.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
 import { AddStudentComponent } from './add-student.component';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AddStudentComponent', () => {
   let component: AddStudentComponent;
@@ -8,7 +10,8 @@ describe('AddStudentComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AddStudentComponent ]
+      declarations: [ AddStudentComponent ],
+      imports: [ HttpClientTestingModule, RouterTestingModule, FormsModule ]
     })
     .compileComponents();
   }));
